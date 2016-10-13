f341203ec87fa263972497bf2cebfe4224e93218
c-calkins <ccalkinsblox@gmail.com>
2016-10-12 21:15:56 -0500

    Need to fix Services

--- App/app.component.ts
+++ App/app.component.ts
@@ -1,18 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero [] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+
+
 
 @Component({
     selector: 'my-app',
@@ -72,13 +63,19 @@ styles: [`
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
     }
-    `]    
+ `]
+ providers: [HeroService]     
 })
-
+import { OnInit } from '@angular/core';
 export class AppComponent {
     title = 'Tour of Heroes';   
-    heroes = HEROES;
-    selectedHero; Hero;
+    heroes: Hero[];
+    selectedHero: Hero;
+    getHeroes(): void {
+        this.heroes = this.heroService.getHeroes();
+    }
+
+    constructor(private heroService: HeroService) {} 
     
     onSelect(hero: Hero): void {
   this.selectedHero = hero;
